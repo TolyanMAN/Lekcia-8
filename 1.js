d9b01b9b7cfff9a6a2adfa439340ae204af3c6ec
Максим Герасименко <76556732+TolyanMAN@users.noreply.github.com>
2021-12-11 15:44:40 +0200

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,21 +1,21 @@
 let rez;
 let calculator = {
-  oneNum: 0,
-  twoNum: 0,
-  read()  //Содержит в себе 2-а числа которые ввёл пользователь
-  {
-    this.oneNum = prompt("Введите первое число: ");
-    this.twoNum = prompt("Введите втрое число: ");
-  },
-  sum()   //Считает сумму 2-х чисел
-  {
-    rez = +this.oneNum + +this.twoNum
-    alert("Сумма: " + rez);
-  },
-  mult()    //Перемножает 2-а числа
-  {
-    rez = this.oneNum * this.twoNum
-    alert("Умножение: " + rez);
+oneNum: 0,
+twoNum: 0,
+read()  //Содержит в себе 2 числа которые ввёл пользователь
+{
+this.oneNum = prompt("Введите первое число: ");
+ this.twoNum = prompt("Введите втрое число: ");
+ },
+ sum()   //Считает сумму 2 чисел
+ {
+ rez = +this.oneNum + +this.twoNum
+ alert("Сумма: " + rez);
+ },
+ mult()    //Перемножает 2 числа
+ {
+  rez = this.oneNum * this.twoNum
+  alert("Умножение: " + rez);
   }
 }
 calculator.read();
